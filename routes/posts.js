374ab6cf7b10e2bd396abc411844dbb345c7a3ca
tomnatt <github@tomnatt.com>
2014-10-09 15:32:50 +0100

    added a view and wired it all in

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var conn = require('../db');
-var ObjectId = require('mongodb').ObjectId;
+var ObjectID = require('mongodb').ObjectID;
+
+router.get('/:id', function(req, res) {
+
+  // method converts string id to MongoId object
+  var id = ObjectID(req.params.id);
+  // pretend we've done some param checking
+
+  conn(function(db, doneCb) {
+    var collection = db.collection('posts');
+    collection.findOne({ _id: id }, function(err, post) {
+      res.render('post', { post: post });
+      doneCb();
+    })
+  })
+
+});
 
 module.exports = router;
