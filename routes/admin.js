f854a4dbbb356205b1a81d00b5875ba032fd32aa
tomnatt <github@tomnatt.com>
2014-10-09 15:39:52 +0100

    added awesome security

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 var conn = require('../db');
 
+router.use(function(req, res, next) {
+  if (req.query.name == 'jack') {
+    next();
+  } else {
+    res.end('no permission');
+  }
+})
+
 router.get('/', function(req, res) {
   res.render('admin');
 });
