50c271a822b97bd098b5aad4ce747037a265824d
tomnatt <github@tomnatt.com>
2014-10-09 15:16:26 +0100

    added connection method

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var conn = require('../db');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  conn(function(db, doneCb) {
+    var posts = db.collection('posts');
+    posts.find().toArray(function(err, posts) {
+      if (err) {
+        // do something!
+      } else {
+        console.log(posts);
+        res.render('index', { title: 'Express' });
+        doneCb();
+      }
+    });
+  })
 });
 
 module.exports = router;
