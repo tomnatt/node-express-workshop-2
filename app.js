4cfb5e09be1c3162fe9a7aedc6c2a36a62a29923
tomnatt <github@tomnatt.com>
2014-10-09 15:37:51 +0100

    initial admin screen

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var posts = require('./routes/posts');
+var adminRoute = require('./routes/admin');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/posts', posts);
+app.use('/admin', adminRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
